1c1d045c313c65cd4588f53e6bc15a80807caad9
AdamPillis <adam.pillis@hotmail.com>
2022-03-31 15:46:20 +0000

    Customise: admin panel

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    """To customise products view via admin panel"""
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """To customise cateogry view via admin panel"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 class Category(models.Model):
     """X"""
+    class Meta:
+        """To fix plural issue relating to django"""
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
