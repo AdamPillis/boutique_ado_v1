081a6fb5df89507e53c3220ad8d8bcecc3ea50c6
AdamPillis <adam.pillis@hotmail.com>
2022-04-07 12:40:37 +0000

    Finished product size logic

--- bag/views.py
+++ bag/views.py
@@ -10,13 +10,28 @@ def add_to_bag(request, item_id):
     """Add a quantity of products specified to the shopping bag"""
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    # setting default size to none and if product has size, set to that instead
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     # storing shopping during their session by creating empty bag object
     bag = request.session.get('bag', {})
-    # add or update quantity of product with same id
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # add or update quantity of product with same id
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
     # update bag within session
     request.session['bag'] = bag
     return redirect(redirect_url)
