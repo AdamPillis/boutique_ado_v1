8ce0f6d9c33afa000944498cfcaff78119b5d0c3
AdamPillis <adam.pillis@hotmail.com>
2022-04-08 10:00:28 +0000

    Added toast messaging functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -7,7 +9,11 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """Add a quantity of products specified to the shopping bag"""
+    """
+    Add a quantity of products specified to the shopping bag
+    """
+    product = Product.objects.get(pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     # setting default size to none and if product has size, set to that instead
@@ -32,6 +38,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
     # update bag within session
     request.session['bag'] = bag
     return redirect(redirect_url)
