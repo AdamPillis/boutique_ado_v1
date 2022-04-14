02ba99f14739641d7a37df82db6695c031449d8b
AdamPillis <adam.pillis@hotmail.com>
2022-04-14 12:10:28 +0000

    Added checkout success logic

---
+++ checkout/__init__.py
@@ -0,0 +1,3 @@
+# telling Django about the custom made class
+# built in apps.py linked with signals
+default_app_config = 'checkout.apps.CheckoutConfig'
--- checkout/models.py
+++ checkout/models.py
@@ -44,7 +44,7 @@ class Order(models.Model):
         Update grand total each time a line item
         is added, accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0  # if all line items are deleted, set to 0 so no error if order total is zero
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
