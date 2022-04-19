aac3c63294b33dd7182f034be6c303236246eed5
AdamPillis <adam.pillis@hotmail.com>
2022-04-19 11:33:48 +0000

    completed webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -30,6 +30,10 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    # variables used incase customer purchases same item/s twice and second order will be identical within database
+    # which would cause issues so stripe id used as it is unique
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -63,6 +67,7 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
+
 
 class OrderLineItem(models.Model):
     """
